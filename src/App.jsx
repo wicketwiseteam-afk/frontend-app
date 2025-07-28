69f9178ae4093db94395f2ee626f22e390cafbd2
Viral Vaghela <viralvaghela8577@gmail.com>
2025-07-28 20:37:04 +0530

    fix: restore LoginPage route in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./Pages/LandingPage";
 import HomePage from "./Pages/HomePage";
-// import LoginPage from "./Pages/LoginPage";
+import LoginPage from "./Pages/LoginPage";
 import RegisterPage from "./Pages/RegisterPage";
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/home" element={<HomePage />} />
-      {/* <Route path="/auth" element={<LoginPage />} /> */}
+      <Route path="/auth" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
     </Routes>
   );
